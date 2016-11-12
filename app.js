c0018ce4cdd5a1bff6d89382ba3d9f139e5f6b9d
Jason Kraft <kraftj3@rpi.edu>
2016-11-12 17:04:54 -0500

    scaffolded basic routing for HTTP requests

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var requests = require('./routes/requests');
 
 var app = express();
 
@@ -30,6 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', users);
+app.use('/logout', users);
+app.use('/register', users);
+app.use('/request', requests);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/requests.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* create a new ride request */
+router.post('/request', function(req, res, next) {
+	
+});
+
+module.exports = router;
