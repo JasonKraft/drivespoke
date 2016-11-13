8b5ad7fb453916a5fd997b24a2d04f7f6059539b
Jason Kraft <kraftj3@rpi.edu>
2016-11-13 04:27:02 -0500

    added error logging in confirmation.js

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -5,17 +5,20 @@ var request = require('../models/requests');
 
 
 router.post('/', function(req, res, next) {
-	
+
 	requestApi('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.address + '&key=AIzaSyCe5Owcj0cRZ6QeR8XHsJOIdsTvvpTbuyU',
 		function (error, response, body) {
-		
+			if (error) {
+				console.log(error);
+			}
+
 		var json_data = JSON.parse(body);
 		latitude = JSON.stringify(json_data.results[0].geometry.location.lat);
 		longitude = JSON.stringify(json_data.results[0].geometry.location.lng);
 		location = "{ lat:" + latitude + ", lng:" + longitude + " }";
-		
 
-		var riderData = { 
+
+		var riderData = {
 			firstName: req.body.passenger_name,
 			lastName: req.body.passenger_name,
 			phone: req.body.passenger_phone_num,
@@ -29,13 +32,13 @@ router.post('/', function(req, res, next) {
 		console.log(riderData);
 
 
-		res.render('confirmation', {phone_number:req.body.passenger_phone_num, 
-									name:req.body.passenger_name, 
-									address: req.body.address, 
+		res.render('confirmation', {phone_number:req.body.passenger_phone_num,
+									name:req.body.passenger_name,
+									address: req.body.address,
 									num_passengers: req.body.num_passengers,
 									google_location: location
 		});
-	});		
+	});
 });
 
 module.exports = router;
