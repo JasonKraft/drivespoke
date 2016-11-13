27b42be096395241aac18e49f63234b2336a2b68
Jason Kraft <kraftj3@rpi.edu>
2016-11-12 19:14:28 -0500

    updated app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/drivespoke');
+var fs = require('fs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
