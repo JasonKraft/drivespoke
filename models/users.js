da5846affd5331574f1eabc213291d632e9c1ecd
uyenuong <uyen.uong33@gmail.com>
2016-11-13 06:11:03 -0500

    fixed some spaccing with the layout

--- models/users.js
+++ models/users.js
@@ -15,6 +15,32 @@ var usersSchema = new Schema ({
 	isOnDuty: {type: Boolean, default: false}
 });
 
+usersSchema.methods.sendSmsNotification = function (message/*, statusCallback */, callback) {
+  var client = new twilio.RestClient(cfg.twilioAccountSid, cfg.twilioAuthToken);
+  var self = this;
+  var options = {
+    to:  self.phone,
+    from: cfg.twilioPhoneNumber,
+    body: message,
+    //statusCallback: statusCallback
+  };
+
+  client.sendMessage(options, function(err, response) {
+    if (err) {
+        console.error(err);
+    } else {
+      var masked = self.phone.substr(0,
+        self.phone.length - 5);
+      masked += '*****';
+      console.log('Message sent to ' + masked);
+    }
+  });
+
+  if (callback) {
+    callback.call(self);
+  }
+};
+
 usersSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('users', usersSchema);
